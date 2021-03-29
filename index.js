6437e262ed9e7bc2732dd4a8a004ad2e58a0e182
beydai <mustafa.beydai@gmail.com>
2021-03-29 02:50:09 +0200

    Use process env insted of config json

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const config = require('./config.json')
+//const config = require('./config.json')
 
 client.on('ready', () => {
     console.log('The client is ready!')
 })
 
-client.login(config.token)
+client.login(process.env.SAKI_TOKEN)
+//client.login(config.token)
