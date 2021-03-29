de93ee1c7bc0d96db0ce9be4e90c64afc9d05ea0
beydai <mustafa.beydai@gmail.com>
2021-03-29 13:20:25 +0200

    fixed command lines

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-//const config = require('./config.json')
+
+const config = require('./config.json')
+const command = require('./command')
 
 client.on('ready', () => {
     console.log('The client is ready!')
+
+    command(client, 'ping', (message) => {
+        message.channel.send('Pong!')
+    })
 })
 
-client.login(process.env.SAKI_TOKEN)
-//client.login(config.token)
+//client.login(process.env.SAKI_TOKEN)
+client.login(config.token)
